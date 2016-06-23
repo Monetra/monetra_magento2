358c098e41bd389bf556c4d4568a18cc387b3130
Greg Weston <gweston@monetra.com>
2016-06-23 09:42:00 -0400

    Updated class in admin system.xml

---
+++ Model/Source/PaymentAction.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Monetra\Monetra\Model\Source;
+
+class PaymentAction implements \Magento\Framework\Option\ArrayInterface
+{
+
+    public function toOptionArray()
+    {
+        return [
+            [
+                'value' => \Monetra\Monetra\Model\ClientTicket::ACTION_AUTHORIZE,
+                'label' => __('Authorize Only'),
+            ],
+            [
+                'value' => \Monetra\Monetra\Model\ClientTicket::ACTION_AUTHORIZE_CAPTURE,
+                'label' => __('Authorize and Capture')
+            ]
+        ];
+    }
+}
